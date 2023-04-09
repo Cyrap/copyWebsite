b8a17ddaf1b139090c15262eddec43eb1eef1d6d
Cyrap <cyrap2001@gmail.com>
2023-04-09 20:24:08 +0800

    push
    push

---
+++ Hope copy/index.js
@@ -0,0 +1,38 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+var box  = document.getElementById('box');
+var down = false;
+function toggleNotifi(){
+  if (down) {
+    box.style.height  = '0px';
+    box.style.opacity = 0;
+    down = false;
+  }else {
+    box.style.height  = '490px';
+    box.style.opacity = 1;
+    down = true;
+  }
+}
+
+var course  = document.getElementById('courses');
+var down = false;
+function side(){
+  if (down) {
+    course.style.height  = '0px';
+    course.style.opacity = 0;
+    course.style.transition = "0.3s";
+    down = false;
+  }else {
+    course.style.height  = '230px';
+    course.style.opacity = 1;
+    course.style.transition = "0.3s";
+    down = true;
+  }
+}
+
